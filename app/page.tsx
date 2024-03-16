96d52b6b7d57c26b9c3de8636a7ea4b5c5fb3f8c
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-03-15 17:03:05 -0700

    render modal start

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,8 @@ import { WagmiProvider, createConfig, createConnector, http } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Main from "./main";
-import { openPopup } from "@usecapsule/react-sdk/dist/modal/utils";
+// import { openPopup } from "@usecapsule/react-sdk/dist/modal/utils";
+import Capsule, { Environment } from "@usecapsule/react-sdk";
 
 const PROJECT_ID = "2c84fab0c5c7fc61e1e0dbc17057cfb7";
 const APP_NAME = "capsule-rainbow-kit";
@@ -30,14 +31,44 @@ export interface DefaultWalletOptions {
 
 export type RainbowWalletOptions = DefaultWalletOptions;
 
+import CapsuleWeb, { CapsuleModal, newTheme } from "@usecapsule/react-sdk";
+import { createRoot } from "react-dom/client";
+
+function renderModal(capsule: CapsuleWeb, appName: string, onCloseArg: () => void): void {
+  const container = document.createElement('div');
+  document.body.appendChild(container); // Add the container to the DOM
+  const root = createRoot(container);
+  const onClose = () => {
+    root.unmount()
+    container.remove(); // Remove the container from the DOM
+    onCloseArg();
+  };
+
+  root.render(
+    <CapsuleModal
+      onClose={onClose}
+      capsule={capsule}
+      isOpen={true}
+      appName={appName}
+      theme={newTheme}
+    />
+  );
+}
+
+
 const capsuleWallet = ({ projectId }: RainbowWalletOptions): Wallet => ({
   id: "capsule",
   name: "Capsule",
   iconUrl: "/static/images/capsule-icon.png",
   iconBackground: "#000000",
   desktop: {
-    getUri: (uri: string) => {
-      openPopup("https://demo.beta.usecapsule.com/");
+    getUri: (_uri: string) => {
+      // will need to fill in actual api key to go through modal flow
+      const API_KEY = '';
+      const capsule = new Capsule(Environment.DEVELOPMENT, API_KEY);
+      renderModal(capsule, 'Rainbow Test', () => {});
+      return '';
+      // openPopup("https://demo.beta.usecapsule.com/");
       // return "https://demo.beta.usecapsule.com/";
     },
   },
